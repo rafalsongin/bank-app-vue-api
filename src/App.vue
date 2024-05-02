c59b5d2e2f0809a3a1e609e0d8566775a2c78558
CezarFlorinel <706431@student.inholland.nl>
2024-05-02 17:05:48 +0200

    imports of fonts

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,13 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 body {
   font-family: 'Open Sans', sans-serif;
-  background-color: #E8C547;
+  background-color: #4D5061;
 }
 </style>
 
