f8204d0618aa3cfc1053465f99b2c3bc80e0d065
IgnasMon <95746420+IgnasMon@users.noreply.github.com>
2024-05-07 01:32:37 +0200

    404 Page + main.css adjustments
    Created a 404 page, for route direction.
    Added co-authors agreed fonts and background color to the main.css

---
+++ src/components/pages/PageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="notfound" class="d-flex justify-content-center align-items-center vh-100">
+    <div class="notfound text-center">
+      <div class="notfound-404">
+        <h1 class="display-1">404</h1>
+      </div>
+      <h2 class="mb-4">Oops! This Page Could Not Be Found</h2>
+      <p class="mb-4">
+        Sorry but the page you are looking for does not exist,
+        have been removed, name changed, or is temporarily unavailable.</p>
+      <a href="/" class="btn btn-primary">Go To Homepage</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#notfound {
+  background-color: #f4f4f4;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.notfound-404 h1 {
+  font-size: 180px;
+  margin: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../components/pages/Home.vue';
+import PageNotFound from "@/components/pages/PageNotFound.vue";
 import ProductList from '../components/products/ProductList.vue';
 import CreateProduct from '../components/products/CreateProduct.vue';
 import EditProduct from '../components/products/EditProduct.vue';
@@ -11,6 +12,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
+    { path: '/404', component: PageNotFound },
     { path: '/products', component: ProductList },
     { path: '/login', component: Login },
     { path: '/createproduct', component: CreateProduct },
