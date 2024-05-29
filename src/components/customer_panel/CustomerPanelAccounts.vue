3f88e9db7cfafae344d711ad4509d50d94b5bf2d
CezarFlorinel <706431@student.inholland.nl>
2024-05-29 17:45:42 +0200

    changes

--- src/components/customer_panel/CustomerPanelAccounts.vue
+++ src/components/customer_panel/CustomerPanelAccounts.vue
@@ -9,8 +9,15 @@
           <div class="card mb-4">
             <div class="card-body">
               <h5 class="card-title">Account Details</h5>
-              <div v-for="account in currentCustomer.accounts" :key="account.iban" class="mb-3">
-                <AccountCard :account="account" @click="selectAccount(account)"></AccountCard>
+              <div
+                v-for="account in currentCustomer.accounts"
+                :key="account.iban"
+                class="mb-3"
+              >
+                <AccountCard
+                  :account="account"
+                  @click="selectAccount(account)"
+                ></AccountCard>
               </div>
             </div>
           </div>
@@ -18,17 +25,31 @@
         <div class="col-md-6" v-if="selectedAccount">
           <div class="card mb-4">
             <div class="card-body">
-              <h5 class="card-title">Transactions for {{ selectedAccount.iban }}</h5>
+              <h5 class="card-title">
+                Transactions for {{ selectedAccount.iban }}
+              </h5>
               <ul class="list-group">
-                <li v-for="transaction in transactions" :key="transaction.id" class="list-group-item">
-                  <strong>Type:</strong> {{ transaction.transaction_type }}<br>
-                  <strong>Amount:</strong> <span :class="amountClass(transaction)">{{ formatAmount(transaction) }}</span><br>
-                  <strong>Date:</strong> {{ formatTimestamp(transaction.timestamp) }}<br>
-                  <strong>From:</strong> {{ transaction.from_account }}<br>
-                  <strong>To:</strong> {{ transaction.to_account }}<br>
-                  <strong>Initialized by:</strong> {{ transaction.initiated_by_account }}
+                <li
+                  v-for="transaction in transactions"
+                  :key="transaction.id"
+                  class="list-group-item"
+                >
+                  <strong>Type:</strong> {{ transaction.transactionType }}<br />
+                  <strong>Amount:</strong>
+                  <span :class="amountClass(transaction)">{{
+                    formatAmount(transaction)
+                  }}</span
+                  ><br />
+                  <strong>Date:</strong>
+                  {{ formatTimestamp(transaction.timestamp) }}<br />
+                  <strong>From:</strong> {{ transaction.fromAccount }}<br />
+                  <strong>To:</strong> {{ transaction.toAccount }}<br />
+                  <strong>Initialized by:</strong>
+                  {{ transaction.initiatedByUser }}
                 </li>
-                <li v-if="!transactions.length" class="list-group-item">No transactions found.</li>
+                <li v-if="!transactions.length" class="list-group-item">
+                  No transactions found.
+                </li>
               </ul>
             </div>
           </div>
@@ -40,14 +61,14 @@
 
 <script>
 import AccountCard from "@/components/customer_panel/PanelComponents/AccountCard.vue";
-import { useTransactionFetchStore } from '@/stores/transactionFetchStore.js';
+import { useTransactionFetchStore } from "@/stores/transactionFetchStore.js";
 
 export default {
   components: {
-    AccountCard
+    AccountCard,
   },
   props: {
-    currentCustomer: Object
+    currentCustomer: Object,
   },
   data() {
     return {
@@ -57,14 +78,20 @@ export default {
   computed: {
     transactions() {
       return useTransactionFetchStore().transactions;
-    }
+    },
   },
   methods: {
     async fetchTransactions(account) {
+      console.log(account);
       try {
-        await useTransactionFetchStore().fetchTransactionsByAccountIban(account.iban);
+        await useTransactionFetchStore().fetchTransactionsByAccountIban(
+          account.iban
+        );
       } catch (error) {
-        console.error('Error when fetching transactions of selected account:', error.message);
+        console.error(
+          "Error when fetching transactions of selected account:",
+          error.message
+        );
       }
     },
     selectAccount(account) {
@@ -73,17 +100,24 @@ export default {
     },
     formatTimestamp(timestamp) {
       const date = new Date(timestamp);
-      return date.toLocaleDateString() + ' ' + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      return (
+        date.toLocaleDateString() +
+        " " +
+        date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+      );
     },
     formatAmount(transaction) {
-      const sign = transaction.from_account === this.selectedAccount.iban ? '-' : '+';
+      const sign =
+        transaction.fromAccount === this.selectedAccount.iban ? "-" : "+";
       const amount = Math.abs(transaction.amount).toFixed(2);
       return `${sign}${amount}`;
     },
     amountClass(transaction) {
-      return transaction.from_account === this.selectedAccount.iban ? 'text-danger font-weight-bold' : 'text-success font-weight-bold';
-    }
-  }
+      return transaction.fromAccount === this.selectedAccount.iban
+        ? "text-danger font-weight-bold"
+        : "text-success font-weight-bold";
+    },
+  },
 };
 </script>
 
