7eec08488fbacc13a9449b632b0e953b032f8825
IgnasMon <95746420+IgnasMon@users.noreply.github.com>
2024-06-03 21:50:48 +0200

    Implemented Customer Settings change
    Implemented Customer Settings page for the customer to change their customer details.

--- src/components/customer_panel/CustomerPanelSettings.vue
+++ src/components/customer_panel/CustomerPanelSettings.vue
@@ -7,43 +7,43 @@
       <div class="mb-3 row">
         <label for="username" class="col-sm-2 col-form-label">Username:</label>
         <div class="col-sm-10">
-          <input type="text" id="username" :value="currentCustomer.username" class="form-control" >
+          <input type="text" id="username" v-model="customerDetails.username" class="form-control">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="email" class="col-sm-2 col-form-label">Email:</label>
         <div class="col-sm-10">
-          <input type="email" id="email" :value="currentCustomer.email" class="form-control" >
+          <input type="email" id="email" v-model="customerDetails.email" class="form-control" disabled>
         </div>
       </div>
       <div class="mb-3 row">
         <label for="firstName" class="col-sm-2 col-form-label">First Name:</label>
         <div class="col-sm-10">
-          <input type="text" id="firstName" :value="currentCustomer.firstName" class="form-control" >
+          <input type="text" id="firstName" v-model="customerDetails.firstName" class="form-control">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="lastName" class="col-sm-2 col-form-label">Last Name:</label>
         <div class="col-sm-10">
-          <input type="text" id="lastName" :value="currentCustomer.lastName" class="form-control" >
+          <input type="text" id="lastName" v-model="customerDetails.lastName" class="form-control">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="phoneNumber" class="col-sm-2 col-form-label">Phone Number:</label>
         <div class="col-sm-10">
-          <input type="text" id="phoneNumber" v-model="currentCustomer.phoneNumber" class="form-control">
+          <input type="text" id="phoneNumber" v-model="customerDetails.phoneNumber" class="form-control">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="bsn" class="col-sm-2 col-form-label">BSN:</label>
         <div class="col-sm-10">
-          <input type="text" id="bsn" :value="currentCustomer.bsn" class="form-control" >
+          <input type="text" id="bsn" v-model="customerDetails.bsn" class="form-control">
         </div>
       </div>
       <div class="mb-3 row">
         <label for="accountApprovalStatus" class="col-sm-2 col-form-label">Account Status:</label>
         <div class="col-sm-10">
-          <input type="text" id="accountApprovalStatus" :value="currentCustomer.accountApprovalStatus" class="form-control" disabled>
+          <input type="text" id="accountApprovalStatus" v-model="customerDetails.accountApprovalStatus" class="form-control" disabled>
         </div>
       </div>
       <div class="mb-3 row">
@@ -60,29 +60,25 @@
 </template>
 
 <script>
+import { useCustomerStore } from '@/stores/customerProfileStore';
+
 export default {
   props: {
     currentCustomer: Object
   },
   data() {
     return {
-      updateCustomerDetails: {
-        username: "",
-        email: "",
-        firstName: "",
-        lastname: "",
-        phoneNumber: "",
-        bsn: "",
-      }
+      customerDetails: { ...this.currentCustomer }
     };
   },
   methods: {
-    updateSettings() {
-      // Function for updating customer settings
-      alert('Settings updated successfully!');
+    async updateSettings() {
+      const customerStore = useCustomerStore();
+      await customerStore.updateCustomerDetails(this.customerDetails);
+      this.$emit('customerUpdated', this.customerDetails); // Emit updated details
     },
     showPasswordChangePanel() {
-      // Function for opening dialogpanel for password change
+      // Function for opening dialog panel for password change
       alert('Open password change panel!');
     }
   }
