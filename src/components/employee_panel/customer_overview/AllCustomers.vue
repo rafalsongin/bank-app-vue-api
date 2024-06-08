f79752d6899140526a711801fb9e5abec3f1feb9
CezarFlorinel <706431@student.inholland.nl>
2024-06-08 16:12:59 +0200

    pagination frontend tables

--- src/components/employee_panel/customer_overview/AllCustomers.vue
+++ src/components/employee_panel/customer_overview/AllCustomers.vue
@@ -11,15 +11,23 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="customer in customers" :key="customer.userId">
+        <tr v-for="customer in paginatedCustomers" :key="customer.userId">
           <td>{{ customer.userId }}</td>
           <td>{{ customer.firstName }} {{ customer.lastName }}</td>
           <td>{{ customer.username }}</td>
-          <td>{{ customer.username }}</td>
+          <td>{{ customer.email }}</td>
           <td>{{ customer.accountApprovalStatus }}</td>
         </tr>
       </tbody>
     </table>
+
+    <div class="pagination">
+      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="nextPage" :disabled="currentPage === totalPages">
+        Next
+      </button>
+    </div>
   </div>
 </template>
 
@@ -28,10 +36,60 @@ export default {
   props: {
     customers: Array,
   },
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 10,
+    };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.customers.length / this.itemsPerPage);
+    },
+    paginatedCustomers() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.customers.slice(start, end);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.pagination button {
+  background-color: #5c80bc;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin: 0 10px;
+  border-radius: 10px;
+}
+
+.pagination button:disabled {
+  background-color: #cccccc;
+  cursor: not-allowed;
+}
+
 .table-responsive {
   width: 100%;
   overflow-x: auto;
